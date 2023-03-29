50ea9e346d7e0824b8044548bc049b317123d50b
SausageMuffins <rwang9098@gmail.com>
2023-03-29 16:25:04 +0800

    Added Accounts class and the relevant TODOs.

---
+++ Aiya_Test_3/app/src/main/java/Accounts/Account.java
@@ -0,0 +1,21 @@
+package Accounts;
+
+// consider using templater and builder pattern also can.
+public abstract class Account {
+    // TODO implement visitor: upvote/downvoting incidents - TBC
+    // TODO implement observer: "subscribe" to incidents - for reported incidents/following incidents.
+
+    // TODO 1: private attributes
+    // TODO 2: abstract methods: eg. getID() - getter, but don't need setter.
+    // TODO 3: normal methods:
+}
+
+// TODO Guest Account extends Account
+// Have limited features - for people who want to just see incidents //
+// do not implement reporting feature
+
+// TODO Normal Account extends Account
+// Have what you expect.
+
+// TODO Verified/Official Accounts for NEA, etc extends Account
+// Able to Resolve Incidents - add updates to incidents.
