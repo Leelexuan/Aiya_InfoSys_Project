9ad24fa0d0ab23c6f6507f1535de4bcba235e76d
SausageMuffins <rwang9098@gmail.com>
2023-03-31 22:24:46 +0800

    Added IncidentLog (Incident Creational Pattern)
    IncidentLog will take log down all reported incidents (name and address) with the date.
    
    Ammended Detail_Input.

---
+++ Aiya_Test_3/app/src/main/java/com/example/aiya_test_3/IncidentLog.java
@@ -0,0 +1,34 @@
+package com.example.aiya_test_3;
+
+public class IncidentLog {
+    // this class implements the singleton (creational) design pattern
+    // purpose: tracking of incidents to ensure accountability.
+
+    private static IncidentLog instance = null;
+    private String log = "";
+
+    // private constructor
+    private IncidentLog() {
+    }
+
+    public static IncidentLog getInstance() {
+        // check if there are any instances of logfile
+        if (instance == null) {
+            // create an instance of incident log file
+            instance = new IncidentLog();
+            // table label formatting
+            // |Date   |Activity Name |Location | but each segment has 15 spaces.
+            instance.INFO(String.format("%-12s","|Date") + "|" + String.format("%-15s","Activity Name") + "|" + String.format("%-15s","Location") + "|");
+        }
+        return instance;
+    }
+
+    // record log
+    public void INFO(String msg) {
+        log += "\n" + msg; // log activities down.
+    }
+
+    public String displayLog(){
+        return log;
+    }
+}
