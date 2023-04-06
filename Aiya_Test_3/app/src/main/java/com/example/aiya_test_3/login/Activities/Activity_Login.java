a4a0582d65c35bb6c3d88f6074076eb815f73ab4
SausageMuffins <rwang9098@gmail.com>
2023-04-06 20:39:43 +0800

    Completed Activity Login EXCEPT for the VALIDATION database portion.

--- Aiya_Test_3/app/src/main/java/com/example/aiya_test_3/login/Activities/Activity_Login.java
+++ Aiya_Test_3/app/src/main/java/com/example/aiya_test_3/login/Activities/Activity_Login.java
@@ -1,20 +1,26 @@
 package com.example.aiya_test_3.login.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.aiya_test_3.R;
+import com.example.aiya_test_3.incidents.Activities.Activity_Incidents;
+import com.example.aiya_test_3.login.UserInput;
 
 public class Activity_Login extends AppCompatActivity {
     // Declaring necessary widgets //
     private EditText password, email;
     private Button loginButton;
+
+    // Declare necessary classes //
+    private UserInput userInput = new UserInput(); // this class is used to verify and validate inputs.
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,19 +40,30 @@ public class Activity_Login extends AppCompatActivity {
                 // Log the attempt to logCat
                 Log.d("Login Button", "User attempted to login");
 
-                // Using Delegation Strategy for clarity //
+                // Get text values from the EditText widgets
+                String emailText = email.getText().toString();
+                String passwordText = password.getText().toString();
 
-                // check if the user input a valid email and password - Verification
-                // TODO - implement a class to check for valid email
+                // DELEGATION STRATEGY //
+                // VERIFY USER INPUT -> IF VERIFIED, VALIDATE WITH DATABASE //
 
-                // check if the user's email and password corresponds to the database - Validation
-                // TODO - implement a class to obtain data from database and cross check with the user input.
+                // VERIFICATION - Check if user input is legal
+                boolean verified = userInput.verify(emailText, passwordText); // verified = true if is legal, false if illegal.
 
-                // Validated User - Proceed back to Incident Activity
-                // TODO - explicit intent to go to incident activity
+                if(!verified){
+                    // display toast message if illegal input
+                    Toast.makeText(Activity_Login.this, "Invalid password/email!", Toast.LENGTH_SHORT).show();
+                } else {
 
-                // Not Validated User (no account found in database) - Prompt sign up.
-                // TODO - a Toast message to prompt user to sign up
+                    // VALIDATION - Cross check user input with database //
+                    boolean validated = userInput.validate(emailText, passwordText);
+
+                    if(validated){
+                        // Go back to Activity_Incident
+                        Intent intent = new Intent(Activity_Login.this, Activity_Incidents.class);
+                        startActivity(intent);
+                    }
+                }
             }
         });
     }
--- Aiya_Test_3/app/src/main/java/com/example/aiya_test_3/login/UserInput.java
+++ Aiya_Test_3/app/src/main/java/com/example/aiya_test_3/login/UserInput.java
@@ -1,6 +1,6 @@
 package com.example.aiya_test_3.login;
 
-public class VerifyUserInput {
+public class UserInput {
     /*DESCRIPTION
     * This class takes in inputs from the login page and checks if the user entered a legal value.
     * It does so by using a static method which can be accessed by every class.
@@ -10,25 +10,22 @@ public class VerifyUserInput {
     * String password - this string must be from the EditText widget with id password from activity_login.xml
     * */
 
-    // ATTRIBUTES //
-    private String email;
-    private String password;
-
-    // CONSTRUCTOR //
-    VerifyUserInput(String email, String password){
-        this.email = email;
-        this.password = password;
-    }
 
     // STATIC METHOD //
-    public static boolean verify(){
+    public static boolean verify(String email, String password){
         // check email //
         // must have @ symbol //
-
+        if(!email.contains("@")){return false;}
 
         // check password //
+        if(!(password.length()<6) && !(password.matches(".*1234567890.*")) && !(password.matches(".*abcdefghijklmnopqrstuvwxyz.*"))){return false;}
         // must include at least 6 characters and have both numbers and password //
 
+        return true; // valid email and password
+    }
+
+    public static boolean validate(String email, String password){
+        // TODO DATABASE: get email and password from database
         return true;
     }
 }
